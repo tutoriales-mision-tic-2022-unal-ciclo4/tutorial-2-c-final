56c2c0f023cc3c3d55032589571a214d5aa51080
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2022-04-25 15:06:55 -0500

    first commit

---
+++ Repositorios/RepositorioInscripcion.py
@@ -0,0 +1,4 @@
+from Repositorios.InterfaceRepositorio import InterfaceRepositorio
+from Modelos.Inscripcion import Inscripcion
+class RepositorioInscripcion(InterfaceRepositorio[Inscripcion]):
+    pass
